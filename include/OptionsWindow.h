441be41130dbb06d8d185a9068ca581de43f806c
KonstigSwe <swekonstig@gmail.com>
2022-01-10 11:49:57 +0100

    Gitproblem trying to fix

---
+++ include/OptionsWindow.h
@@ -0,0 +1,18 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include <iostream>
+#include <fstream>
+namespace blimp {
+	class Optionswindow :public wxFrame {
+	public:
+		Optionswindow();
+
+	private:
+		wxWindowID autoplayID = wxWindow::NewControlId();
+		void AutoplayCheckboxClicked(wxCommandEvent& event);
+		void write(bool input);
+		bool read();
+	};
+}
