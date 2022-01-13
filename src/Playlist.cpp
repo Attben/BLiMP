ecef9f8c1b201bc95de6b8253547d70fe52a2591
Attben <72492246+Attben@users.noreply.github.com>
2022-01-13 10:03:40 +0100

    Add items to playlist

--- src/Playlist.cpp
+++ src/Playlist.cpp
@@ -1,18 +1,25 @@
 #include "../include/Playlist.h"
+#include <wx/filename.h>
 
 namespace blimp {
 	Playlist::Playlist(wxWindow* parent, wxWindowID id)
-		: wxListCtrl{ parent, id },
+		: wxListCtrl{ parent, id, wxDefaultPosition,  wxDefaultSize, wxLC_REPORT},
 		_currentItem{0L}
-	{}
+	{
+		AppendColumn(_("File"), wxLIST_FORMAT_LEFT, /*wxLIST_AUTOSIZE_USEHEADER*/305);
+		AppendColumn(_("Length"), wxLIST_FORMAT_CENTER, 75);
+	}
 
-	void Playlist::Append(const wxString& item) {
-		wxListItem newItem;
-		newItem.SetAlign(wxLIST_FORMAT_LEFT);
-		int ID = this->GetItemCount();
-		newItem.SetId(ID);
-		newItem.SetData(new wxString{ item });
-		InsertItem(newItem);
+	void Playlist::Append(const wxString& path) {
+		wxListItem item;
+		item.SetAlign(wxLIST_FORMAT_LEFT);
+		int ID = GetItemCount();
+		item.SetId(ID);
+		item.SetMask(wxLIST_MASK_DATA);
+		item.SetData(new wxString{ path });
+		InsertItem(item);
+
+		this->SetItem(ID, 0, wxFileName(path).GetName());
 	}
 
 	wxString Playlist::GetNextItem() {
@@ -20,7 +27,7 @@ namespace blimp {
 			return "";
 		}
 		else {
-			return GetItemText(++_currentItem);
+			return GetItemText(_currentItem);
 		}
 	}
 }
